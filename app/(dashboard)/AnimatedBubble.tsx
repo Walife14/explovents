c5bd93e28efdc20b8cc30d27be319ac38f6e9b43
Lucas <lferon14@gmail.com>
2024-05-01 14:36:24 +0100

    Fixed issue with AnimatedBubble component where it would correctly use colour prop

--- app/(dashboard)/AnimatedBubble.tsx
+++ app/(dashboard)/AnimatedBubble.tsx
@@ -1,26 +1,45 @@
 import { motion } from "framer-motion"
 
 type Props = {
-    color: string;
+    color: number;
     delay: number;
     position: string;
 }
 
 function AnimatedBubble({ color, delay, position }: Props) {
     return (
-        <motion.div
-            className={`absolute w-[20%] aspect-square bg-${color} rounded-full mix-blend-screen -z-10 ${position} blur-2xl`}
-            animate={{
-                x: [0, 0, -20, 20, 0],
-                y: [0, 20, 0, -20, 0],
-            }}
-            transition={{
-                duration: 4,
-                ease: 'linear',
-                repeat: Infinity,
-                delay: delay,
-            }}
-        />
+        <>
+            {color === 1 && (
+                <motion.div
+                    className={`absolute w-[20%] aspect-square bg-triary rounded-full mix-blend-screen -z-10 ${position} blur-2xl`}
+                    animate={{
+                        x: [0, 0, -40, 20, 0],
+                        y: [0, 40, 0, -40, 0],
+                    }}
+                    transition={{
+                        duration: 5,
+                        ease: 'linear',
+                        repeat: Infinity,
+                        delay: delay,
+                    }}
+                />
+            )}
+            {color === 2 && (
+                <motion.div
+                    className={`absolute w-[20%] aspect-square bg-secondary rounded-full mix-blend-screen -z-10 ${position} blur-2xl`}
+                    animate={{
+                        x: [0, 0, -40, 20, 0],
+                        y: [0, 40, 0, -40, 0],
+                    }}
+                    transition={{
+                        duration: 5,
+                        ease: 'linear',
+                        repeat: Infinity,
+                        delay: delay,
+                    }}
+                />
+            )}
+        </>
     );
 }
 
--- app/(dashboard)/Search.tsx
+++ app/(dashboard)/Search.tsx
@@ -30,19 +30,20 @@ function Search({ }: Props) {
 
     return (
         <div className="w-5/6 mx-auto space-y-10 relative mt-40">
-            <AnimatedBubble color={'triary-light'} delay={0} position={'top-[5%] -left-[5%]'} />
-            <AnimatedBubble color={'triary-light'} delay={0.25} position={'-top-[5%] left-[10%]'} />
-            <AnimatedBubble color={'triary-light'} delay={1} position={'-top-[5%] left-[25%]'} />
-            <AnimatedBubble color={'triary-light'} delay={0.75} position={'top-[50%] left-[25%]'} />
-            <AnimatedBubble color={'triary-light'} delay={0.25} position={'-top-[5%] left-[40%]'} />
-            <AnimatedBubble color={'secondary-light'} delay={1} position={'top-[50%] left-[40%]'} />
-            <AnimatedBubble color={'triary-light'} delay={0.2} position={'-top-[5%] right-[25%]'} />
-            <AnimatedBubble color={'triary-light'} delay={0.6} position={'top-[50%] right-[25%]'} />
-            <AnimatedBubble color={'triary-light'} delay={0.4} position={'-top-[5%] right-[10%]'} />
-            <AnimatedBubble color={'triary-light'} delay={0.8} position={'top-[5%] -right-[5%]'} />
+            <AnimatedBubble color={1} delay={0} position={'top-[5%] -left-[5%]'} />
+            <AnimatedBubble color={1} delay={0.25} position={'-top-[5%] left-[10%]'} />
+            <AnimatedBubble color={1} delay={1} position={'-top-[5%] left-[25%]'} />
+            <AnimatedBubble color={1} delay={0.75} position={'top-[50%] left-[25%]'} />
+            <AnimatedBubble color={1} delay={0.25} position={'-top-[5%] left-[40%]'} />
+            <AnimatedBubble color={2} delay={1} position={'top-[40%] left-[40%]'} />
+            <AnimatedBubble color={2} delay={0.5} position={'top-[10%] left-[40%]'} />
+            <AnimatedBubble color={1} delay={0.2} position={'-top-[5%] right-[25%]'} />
+            <AnimatedBubble color={1} delay={0.6} position={'top-[50%] right-[25%]'} />
+            <AnimatedBubble color={1} delay={0.4} position={'-top-[5%] right-[10%]'} />
+            <AnimatedBubble color={1} delay={0.8} position={'top-[5%] -right-[5%]'} />
             <h1 className="text-3xl font-black text-center">We've got you covered</h1>
             <form onSubmit={(e) => handleSubmit(e, country, city, selectedDate)}>
-                <div className="flex gap-x-1 p-1 bg-secondary text-2xl rounded-md">
+                <div className="flex gap-x-1 p-1 bg-triary text-2xl rounded-md">
                     <div className="flex-1 flex">
                         <label htmlFor="countryInput" hidden>Select Country</label>
                         <div className="h-auto aspect-square rounded-l-md bg-white">
