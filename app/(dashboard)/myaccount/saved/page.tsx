36c839dce76c218b3ff3d9a3d16134b0b84fabfe
Lucas <lferon14@gmail.com>
2024-06-09 04:04:47 +0100

    Added navbar open new container + mobile responsive
    Added empty saved events message to let users know they haven't saved any events

--- app/(dashboard)/myaccount/saved/page.tsx
+++ app/(dashboard)/myaccount/saved/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
 // components
 import EventCard from "@components/EventCard/EventCard";
 import BackBtn from "@/app/components/BackBtn/BackBtn";
-import { useEffect, useState } from "react";
+import LoadingSpinner from "@/app/components/LoadingSpinner/LoadingSpinner";
 
 // types and interfaces
 import { IEvent } from "@/app/interfaces/IEvent";
@@ -26,7 +29,7 @@ function Saved({}: Props) {
         const json = await res.json();
 
         if (json) {
-          console.log(json);
+          // console.log(json);
           setSavedEvents(json);
         }
       } catch (error) {
@@ -46,19 +49,52 @@ function Saved({}: Props) {
           <BackBtn />
           <h1 className="text-primary text-center">Saved Events</h1>
         </div>
-        {!loading && (
+        {!loading ? (
           <div>
-            {savedEvents.map((event: IEvent, index) => (
-              <EventCard
-                key={index}
-                title={event.title}
-                description={event.description}
-                price={60}
-                url={`/event/${event.id}`}
-                image={event.banner_image}
-              />
-            ))}
+            {savedEvents.length > 0 ? (
+              savedEvents.map((event: IEvent, index) => (
+                <EventCard
+                  key={index}
+                  title={event.title}
+                  description={event.description}
+                  price={60}
+                  url={`/event/${event.id}`}
+                  image={event.banner_image}
+                />
+              ))
+            ) : (
+              <div className="h-[50vh] flex flex-col gap-y-8 items-center justify-center">
+                <svg
+                  className="w-16 h-16 fill-dark-gray"
+                  fill="#000000"
+                  viewBox="0 0 256 256"
+                  id="Flat"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></g>
+                  <g id="SVGRepo_iconCarrier">
+                    {" "}
+                    <path d="M108,116a8,8,0,1,1-8-8A7.99993,7.99993,0,0,1,108,116Zm48-8a8,8,0,1,0,8,8A7.99993,7.99993,0,0,0,156,108Zm64,12v96a4.00007,4.00007,0,0,1-6.5332,3.0957L186.667,197.168,159.86621,219.0957a4.00069,4.00069,0,0,1-5.06641,0L128,197.168,101.2002,219.0957a4.00069,4.00069,0,0,1-5.06641,0L69.333,197.168,42.5332,219.0957A4,4,0,0,1,36,216V120a92,92,0,0,1,184,0Zm-8,0a84,84,0,0,0-168,0v87.55908L66.7998,188.9043a4.00069,4.00069,0,0,1,5.06641,0L98.667,210.832,125.4668,188.9043a4.00025,4.00025,0,0,1,5.0664,0L157.333,210.832l26.80078-21.92773a4.00069,4.00069,0,0,1,5.06641,0L212,207.55908Z"></path>{" "}
+                  </g>
+                </svg>
+                <p className="md:w-2/3 text-center">
+                  Unfortunately, it seems you haven&apos;t saved any events yet.
+                  Fancy exploring some of our top picks? Click{" "}
+                  <Link className="underline" href="/explore">
+                    here
+                  </Link>{" "}
+                  to get started!
+                </p>
+              </div>
+            )}
           </div>
+        ) : (
+          <LoadingSpinner />
         )}
       </div>
     </main>
--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,117 +1,168 @@
-"use client"
+"use client";
 
-import { usePathname } from 'next/navigation';
-import { createClient } from '@/utils/supabase/client';
-import { useCallback, useEffect, useState } from 'react';
-import { User } from '@supabase/supabase-js';
-import Link from 'next/link';
+import { usePathname } from "next/navigation";
+import { createClient } from "@/utils/supabase/client";
+import { useCallback, useEffect, useState } from "react";
+import { User } from "@supabase/supabase-js";
+import Link from "next/link";
 
 // fonts
-import { Bowlby_One_SC } from 'next/font/google'
-import { logout } from '@/app/(auth)/actions';
+import { Bowlby_One_SC } from "next/font/google";
+import { logout } from "@/app/(auth)/actions";
 
 const bowlby = Bowlby_One_SC({
-    subsets: ["latin"],
-    weight: '400'
+  subsets: ["latin"],
+  weight: "400",
 });
 
-type Props = {}
+type Props = {};
 
-function Navbar({ }: Props) {
-    const pathname = usePathname()
-    const supabase = createClient()
-    const [user, setUser] = useState<User | null>(null)
-    const [isLoading, setIsLoading] = useState<boolean>(true)
-    const [isMyAccountNavOpen, setIsMyAccountNavOpen] = useState<boolean>(false)
+function Navbar({}: Props) {
+  const pathname = usePathname();
+  const supabase = createClient();
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isMyAccountNavOpen, setIsMyAccountNavOpen] = useState<boolean>(false);
 
-    const handleLogout = async () => {
-        try {
-            await logout()
-            window.location.reload()
-        } catch (error) {
-            console.error(error)
-        }
+  const handleLogout = async () => {
+    try {
+      await logout();
+      window.location.reload();
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-    const fetchUser = useCallback(async () => {
-        try {
-            const { data, error } = await supabase.auth.getUser()
-            if (data?.user) {
-                setUser(data.user)
-            }
-        } catch (error) {
-            // console.error('error fetching user: ', error)
-        } finally {
-            setIsLoading(false)
-        }
-    }, [])
+  const fetchUser = useCallback(async () => {
+    try {
+      const { data, error } = await supabase.auth.getUser();
+      if (data?.user) {
+        setUser(data.user);
+      }
+    } catch (error) {
+      // console.error('error fetching user: ', error)
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
 
-    useEffect(() => {
-        fetchUser()
-        setIsMyAccountNavOpen(false)
-    }, [fetchUser, pathname])
+  useEffect(() => {
+    fetchUser();
+    setIsMyAccountNavOpen(false);
+  }, [fetchUser, pathname]);
 
-    return (
-        <header className='z-50'>
-            <nav>
-                <div className='flex items-end justify-between mx-4 md:w-5/6 md:mx-auto py-8 flex-wrap'>
-                    <Link href="/" className={`${bowlby.className} text-primary text-3xl`} data-testid="logo">Explovents</Link>
-                    {/* <div> */}
-                    {user && (
-                        <span className='hidden md:inline-block text-dark-gray'>What&apos;s up, {user.email}!</span>
-                    )}
+  return (
+    <header className="z-50">
+      <nav>
+        <div className="flex items-end justify-between mx-4 md:w-5/6 md:mx-auto py-8 flex-wrap">
+          <Link
+            href="/"
+            className={`${bowlby.className} text-primary text-3xl`}
+            data-testid="logo"
+          >
+            Explovents
+          </Link>
+          {/* <div> */}
+          {user && (
+            <span className="hidden md:inline-block text-dark-gray">
+              What&apos;s up, {user.email}!
+            </span>
+          )}
 
-
-                    <div className='basis-full flex justify-between py-4 font-semibold'>
-                        <Link className={`flex items-center gap-x-2 rounded-full py-2 px-3 hover:ring-2 hover:ring-secondary group ${pathname === '/explore' ? 'ring-2 ring-secondary' : ''}`} href="/explore">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1" stroke="currentColor" className={`w-6 h-6 group-hover:fill-yellow-400 group-hover:stroke-yellow-400 ${pathname === '/explore' ? 'fill-yellow-400 stroke-yellow-400' : ''}`}>
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-                            </svg>
-                            Events
-                        </Link>
-                        <ul className='flex items-center gap-x-4'>
-                            {!isLoading && (
-                                <>
-                                    {!user ? (
-                                        <>
-                                            <li>
-                                                <Link href="/login">
-                                                    Login
-                                                </Link>
-                                            </li>
-                                            <li>
-                                                <Link href="/register">
-                                                    Register
-                                                </Link>
-                                            </li>
-                                        </>
-                                    ) : (
-                                        <li>
-                                            <div className='relative'>
-                                                <button onClick={() => setIsMyAccountNavOpen(!isMyAccountNavOpen)} className={`${isMyAccountNavOpen && 'text-primary'} transition-colors duration-100`}>
-                                                    My Account
-                                                </button>
-                                                <div className={`absolute flex flex-col w-screen -mr-4 md:w-max right-0 mt-1 rounded-md p-4 bg-white border shadow-md text-center gap-y-4 ${!isMyAccountNavOpen && 'hidden'}`}>
-                                                    <span className='md:hidden text-dark-gray'>What&apos;s up, {user.email}!</span>
-                                                    <Link href="/myaccount">
-                                                        Manage Account
-                                                    </Link>
-                                                    <button onClick={handleLogout}>
-                                                        Logout
-                                                    </button>
-                                                </div>
-                                            </div>
-                                        </li>
-                                    )}
-                                </>
-                            )}
-                        </ul>
-                    </div>
-                    {/* </div> */}
-                </div>
-            </nav>
-        </header>
-    )
+          <div className="basis-full flex justify-between py-4 font-semibold">
+            <Link
+              className={`flex items-center gap-x-2 rounded-full py-2 px-3 hover:ring-2 hover:ring-secondary group ${
+                pathname === "/explore" ? "ring-2 ring-secondary" : ""
+              }`}
+              href="/explore"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1"
+                stroke="currentColor"
+                className={`w-6 h-6 group-hover:fill-yellow-400 group-hover:stroke-yellow-400 ${
+                  pathname === "/explore"
+                    ? "fill-yellow-400 stroke-yellow-400"
+                    : ""
+                }`}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+                />
+              </svg>
+              Events
+            </Link>
+            <ul className="flex items-center gap-x-4">
+              {!isLoading && (
+                <>
+                  {!user ? (
+                    <>
+                      <li>
+                        <Link href="/login">Login</Link>
+                      </li>
+                      <li>
+                        <Link href="/register">Register</Link>
+                      </li>
+                    </>
+                  ) : (
+                    <li>
+                      <div className="relative">
+                        <button
+                          onClick={() =>
+                            setIsMyAccountNavOpen(!isMyAccountNavOpen)
+                          }
+                          className={`${
+                            isMyAccountNavOpen && "text-primary"
+                          } transition-colors duration-100`}
+                        >
+                          My Account
+                        </button>
+                        <div
+                          className={`absolute w-screen -mr-4 md:w-max md:min-w-[300px] right-0 p-4 ${
+                            !isMyAccountNavOpen && "hidden"
+                          }`}
+                        >
+                          <div className="flex flex-col bg-white gap-y-4 p-4 shadow-[0_0_6px] shadow-triary text-center">
+                            <span className="md:hidden text-dark-gray">
+                              What&apos;s up, {user.email}!
+                            </span>
+                            <ul className="flex flex-col gap-y-2 font-bold">
+                              <li>
+                                <Link href="/myaccount">Manage Account</Link>
+                              </li>
+                              <li>
+                                <Link href="/myaccount/saved">
+                                  Saved Events
+                                </Link>
+                              </li>
+                              <li>
+                                <Link href="#">Booked Events</Link>
+                              </li>
+                            </ul>
+                            <button
+                              className="border border-secondary text-secondary py-2 rounded-md"
+                              onClick={handleLogout}
+                            >
+                              Logout
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                  )}
+                </>
+              )}
+            </ul>
+          </div>
+          {/* </div> */}
+        </div>
+      </nav>
+    </header>
+  );
 }
 
-export default Navbar
+export default Navbar;
