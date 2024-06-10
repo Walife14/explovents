8c3ea2c128397138a321292c1c102b794e08d59b
Lucas <lferon14@gmail.com>
2024-06-10 02:50:17 +0100

    Added additional button styling such as hover, active and focus states

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -1,20 +1,24 @@
-
 type Props = {
-    text: string;
-    type?: "submit" | "reset" | "button" | undefined;
-    largeText?: boolean;
-    nonFullWidth?: boolean;
+  text: string;
+  type?: "submit" | "reset" | "button" | undefined;
+  largeText?: boolean;
+  nonFullWidth?: boolean;
+  disabled?: boolean;
+};
+
+function Button({ text, largeText, nonFullWidth, type, disabled }: Props) {
+  return (
+    <button
+      className={`bg-secondary font-semibold text-white py-2 my-4 rounded-md transition-all hover:scale-[1.05] hover:bg-secondary-dark active:scale-[0.98] active:bg-secondary-light focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-secondary disabled:bg-dark-gray
+                ${nonFullWidth ? "px-20" : "w-full"} mx-auto ${
+        largeText ? "text-2xl" : ""
+      }`}
+      type={type || "button"}
+      disabled={disabled || false}
+    >
+      {text}
+    </button>
+  );
 }
 
-function Button({ text, largeText, nonFullWidth, type }: Props) {
-    return (
-        <button
-            className={`bg-secondary font-semibold text-white py-2 my-4 rounded-md ${nonFullWidth ? 'px-20' : 'w-full'} mx-auto ${largeText ? 'text-2xl' : ''}`}
-            type={type || "button"}
-        >
-            {text}
-        </button>
-    )
-}
-
-export default Button
+export default Button;
