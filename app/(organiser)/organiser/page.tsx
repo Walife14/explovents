4270e94e88c17e1c74a237608c961e905829611a
Lucas <lferon14@gmail.com>
2025-05-14 10:16:47 +0100

    ADDED Explovents favicon ADDED additional content into organiser landing section

--- app/(organiser)/organiser/page.tsx
+++ app/(organiser)/organiser/page.tsx
@@ -1,32 +1,42 @@
-import LinkBtn from '@/app/components/LinkBtn/LinkBtn'
+import Link from 'next/link'
 import React from 'react'
 
 // components
+import LinkBtn from '@/app/components/LinkBtn/LinkBtn'
+
+// icons
+import { ChevronDoubleDownIcon } from '@heroicons/react/24/solid'
 
 type Props = {}
 
 function page({ }: Props) {
     return (
         <>
-            <div className='px-4'>
-                <h1 className='text-center'>Welcome to Explovents Organiser Hub</h1>
-
-                <div className='flex flex-col gap-y-2 my-10'>
-                    {/* <LinkBtn text={'Apply for Organiser Account'} textxl={true} href='/organiser/register'></LinkBtn> */}
-                    {/* <LinkBtn text={'Login to Organiser Account'} href='/organiser/login' textxl transparent></LinkBtn> */}
-                </div>
+            <div className='px-4 md:w-5/6 md:mx-auto'>
+                <h1 className='text-center mb-4'>Welcome to the Explovents
+                    <br />Organiser Hub
+                </h1>
 
                 <div>
-                    <h2 className='mb-4'>Why should I register for an organiser account?</h2>
-                    <div className='flex flex-col gap-y-2'>
-                        <p>
-                            By creating an organiser account on Explovents, you&apos;ll unlock the tools needed to manage your events effortlessly.
-                            From promoting events and tracking ticket sales to engaging with attendees, our platform provides everything you need to run successful events.
-                        </p>
-                        <p>
-                            Gain access to real-time analytics, streamline bookings, and showcase your brand to a wide audience.
-                            Get started today and watch your events thrive!
-                        </p>
+                    <h2 className='mb-4'>Interested in hosting events with Explovents?</h2>
+                    <div className='flex flex-col items-center gap-y-2 text-white p-4 bg-[#16397A]'>
+                        <h3>
+                            Get everything you need to run successful events in one place
+                        </h3>
+
+                        <ul className='pl-5 space-y-1 text-center'>
+                            <li>
+                                Advertise to thousands of potential attendees
+                            </li>
+                            <li>Track ticket sales in real-time</li>
+                            <li>Manage bookings effortlessly</li>
+                            <li>Access powerful analytics</li>
+                        </ul>
+                    </div>
+                    <ChevronDoubleDownIcon className='size-6 my-4 mx-auto'></ChevronDoubleDownIcon>
+                    <div className='flex flex-col items-center gap-y-2'>
+                        <LinkBtn text={'Get Started'} href='/organiser/register'></LinkBtn>
+                        <span className='flex gap-x-2'>Already an organiser? <Link className='underline' href='/organiser/login'>Log in</Link></span>
                     </div>
                 </div>
             </div>
